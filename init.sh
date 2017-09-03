372bfe9a02c75a0debfa1277570151b648234565
Bruno Agostini <brunogsa.92@gmail.com>
2017-09-03 16:55:19 -0300

    Fix a typo on init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo add-apt-repository  ppa:klaus-vormweg/awesome -y
 sudo apt update
 sudo apt install  awesome -y
 
-sudo mp -p ~/.config/awesome/
+sudo mkdir -p ~/.config/awesome/
 sudo ln -s rc.lua ~/.config/awesome/
 sudo ln -s mytheme.lua ~/.config/awesome/
 
