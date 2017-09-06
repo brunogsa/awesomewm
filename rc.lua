e896879ddba144b6b6408c954dc920ae1fd61f04
Bruno Agostini <bagostini@daitangroup.com>
2017-09-06 14:38:04 -0300

    Better Menu
    Details:
      - Better names
      - Removed unnecessary sections

--- rc.lua
+++ rc.lua
@@ -107,18 +107,16 @@ end
 -- {{{ Menu
 -- Create a launcher widget and a main menu
 myawesomemenu = {
-    { "hotkeys", function() return false, hotkeys_popup.show_help end },
-    { "manual", terminal .. " -e man awesome" },
-    { "edit config", editor_cmd .. " " .. awesome.conffile },
-    { "restart", awesome.restart },
-    { "quit", function() awesome.quit() end }
+    { "Hotkeys", function() return false, hotkeys_popup.show_help end },
+    { "Manual", terminal .. " -e man awesome" },
+    { "Quit", function() awesome.quit() end }
 }
 
 mymainmenu = awful.menu({
     items = {
-        { "awesome", myawesomemenu, beautiful.awesome_icon },
-        { "Debian", debian.menu.Debian_menu.Debian },
-        { "open terminal", terminal }
+        { "Terminal", terminal },
+        { "Awesome", myawesomemenu },
+        { "Debian", debian.menu.Debian_menu.Debian }
     }
 })
 
